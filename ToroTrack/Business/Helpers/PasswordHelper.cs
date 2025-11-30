698e5382e57c1de56058ded9a1ad55f010a4a447
Mathabo Motaung <moatung20mathabo>
2025-11-30 06:10:00 +0200

    Updated business logic for the forgot password page

---
+++ ToroTrack/Business/Helpers/PasswordHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace ToroTrack.Business.Helpers
+{
+    // Why: Centralized password generation logic to be reused by InviteService and ForgotPassword
+    public static class PasswordHelper
+    {
+        public static string GenerateRandomPassword()
+        {
+            // Why: Microsoft Identity requires at least 1 Upper, 1 Lower, 1 Digit, 1 Non-Alphanumeric
+            const string lowers = "abcdefghijklmnopqrstuvwxyz";
+            const string uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const string digits = "0123456789";
+            const string specials = "!@#$%^&*";
+
+            var allChars = lowers + uppers + digits + specials;
+            var random = new Random();
+            var password = new StringBuilder();
+
+            // Ensure one of each requirement is met first
+            password.Append(lowers[random.Next(lowers.Length)]);
+            password.Append(uppers[random.Next(uppers.Length)]);
+            password.Append(digits[random.Next(digits.Length)]);
+            password.Append(specials[random.Next(specials.Length)]);
+
+            // Fill the rest to reach length of 12
+            while (password.Length < 12)
+            {
+                password.Append(allChars[random.Next(allChars.Length)]);
+            }
+
+            // Shuffle the result so the required chars aren't always at the start
+            return new string(password.ToString().OrderBy(c => random.Next()).ToArray());
+        }
+    }
+}
--- ToroTrack/Business/Services/InviteService.cs
+++ ToroTrack/Business/Services/InviteService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using System.Text;
+using ToroTrack.Business.Helpers; // Added for PasswordHelper
 using ToroTrack.Data;
 using ToroTrack.Data.Entities;
 using ToroTrack.Data.Repositories;
@@ -59,8 +60,8 @@ namespace ToroTrack.Business.Services
 
             try
             {
-                // Generate Secure Password
-                string temporaryPassword = GenerateRandomPassword();
+                // Why: Reuse the centralized password generation logic
+                string temporaryPassword = PasswordHelper.GenerateRandomPassword();
 
                 // Create Identity User
                 var newUser = new ApplicationUser
@@ -142,34 +143,6 @@ namespace ToroTrack.Business.Services
                 </div>";
 
             await _emailSender.SendEmailAsync(email, subject, htmlMessage);
-        }
-
-        private string GenerateRandomPassword()
-        {
-            // Why: Microsoft Identity requires at least 1 Upper, 1 Lower, 1 Digit, 1 Non-Alphanumeric
-            const string lowers = "abcdefghijklmnopqrstuvwxyz";
-            const string uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            const string digits = "0123456789";
-            const string specials = "!@#$%^&*";
-
-            var allChars = lowers + uppers + digits + specials;
-            var random = new Random();
-            var password = new StringBuilder();
-
-            // Ensure one of each requirement is met first
-            password.Append(lowers[random.Next(lowers.Length)]);
-            password.Append(uppers[random.Next(uppers.Length)]);
-            password.Append(digits[random.Next(digits.Length)]);
-            password.Append(specials[random.Next(specials.Length)]);
-
-            // Fill the rest to reach length of 12
-            while (password.Length < 12)
-            {
-                password.Append(allChars[random.Next(allChars.Length)]);
-            }
-
-            // Shuffle the result so the required chars aren't always at the start
-            return new string(password.ToString().OrderBy(c => random.Next()).ToArray());
         }
 
         private string ParseFirstName(string? fullName)
